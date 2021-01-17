572bb5be2e62d8053f01f4f65711e91b44dfab4f
Elie Elia <elie.elia@mail.mcgill.ca>
2021-01-17 23:01:38 +0200

    Heroku fix

--- src/gateways/gallery-api.js
+++ src/gateways/gallery-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://shopify-application-imagerepo.herokuapp.com/";
+const url = "http://shopify-application-imagerepo.herokuapp.com/";
 export const api = axios.create({
   baseURL: url
 });
